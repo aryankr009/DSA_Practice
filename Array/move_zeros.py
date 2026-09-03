2af80fcd2dd1ee09aee2a46742383b49d0b21c5f
Aryan Kumar <aryankumar2394@gmail.com>
2026-09-03 09:22:36 +0530

    Solution of Array

---
+++ Array/move_zeros.py
@@ -0,0 +1,14 @@
+def move_zeros(arr):
+    pos = 0
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[pos], arr[i] = arr[i], arr[pos]
+            pos += 1
+
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [0, 1, 0, 3, 12]
+    print(move_zeros(arr))
